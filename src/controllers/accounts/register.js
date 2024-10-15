5e0ec30684560ee6c993a9a1b1e55dc484ce9af1
đạt hoàng <153180946+datzero-9@users.noreply.github.com>
2024-10-15 20:23:45 +0700

    Update register.js

--- src/controllers/accounts/register.js
+++ src/controllers/accounts/register.js
@@ -2,6 +2,7 @@
 const Account = require('../../models/accounts')
 const session = require('express-session');
 const register = (req, res) => {
+  console.log('Request: GET')
   res.render('register', { layout: false })
 }
 
@@ -9,6 +10,7 @@ const createRegister = async (req, res) => {
   try {
     const { name, username, password } = req.body;
     // Tìm tài khoản đã tồn tại
+    console.log('Request: POST : Tạo tài khoản)
     const existingAccount = await Account.findOne({ username });
     if (existingAccount) {
       return res.status(409).json({ message: 'Tài khoản đã tồn tại' });
@@ -30,33 +32,20 @@ const createRegister = async (req, res) => {
 
 
 const login = (req, res) => {
+  console.log('Request : GET')
   res.render('login', { layout: false })
 }
 
 const dangnhap = async (req, res) => {
   try {
+    console.log('Request : POST : Đăng nhập')
     const username = req.body.username;
     const password = req.body.password;
     // Tìm tài khoản đã tồn tại
     const existingAccount = await Account.findOne({ username: username, password: password });
 
     if (existingAccount) {
-      req.session.user = {
-        id: existingAccount._id,
-        name: existingAccount.name,
-        role: existingAccount.role,
-      };
-      console.log(`Người dùng: ${existingAccount.name} đã Đăng nhập`);
-      res.redirect(`/admin?username=${existingAccount.name}`);
-    } else {
-      res.render('login', { layout: false, mess: true })
-    }
-  } catch (error) {
-    console.error('Error creating account:', error);
-    res.status(500).json({ message: 'Có lỗi xảy ra khi tạo tài khoản' });
-  }
-}
-const logout =  (req, res) => {
+      req.sesxuất')
   req.session.destroy((err) => {
     if (err) {
       console.error('Error destroying session:', err);

ff8b31c712fb4c44bd868dc907363f528c6b08f6
dat hoang <datht.22ite@vku.udn.vn>
2025-02-27 20:21:16 +0700

    pay online

--- src/controllers/checkout/checkout.js
+++ src/controllers/checkout/checkout.js
@@ -162,12 +162,16 @@ const Callback = async (req, res) => {
 
             try {
                 axios.post('https://restfulapi-aci6.onrender.com/api/checkout', checkout)
-                // axios.post('https://4fe8-116-105-208-170.ngrok-free.app/api/checkout', checkout)
+                    // axios.post('https://4fe8-116-105-208-170.ngrok-free.app/api/checkout', checkout)
                     .then((res) => {
                         try {
                             axios.delete(`https://restfulapi-aci6.onrender.com/api/deleteAllCart/${checkout.idUser}`)
-                            // axios.delete(`https://4fe8-116-105-208-170.ngrok-free.app/api/deleteAllCart/${checkout.idUser}`)
+                                // axios.delete(`https://4fe8-116-105-208-170.ngrok-free.app/api/deleteAllCart/${checkout.idUser}`)
                                 .then((res) => {
+                                    axios.post(`https://a9aa-116-110-94-16.ngrok-free.app/webhook/comfirm-order`, {})
+                                        .then((res) => {
+                                            console.log('gọi đến webhook thành công')
+                                        })
                                     console.log(res.data)
                                 })
                         } catch (error) {
